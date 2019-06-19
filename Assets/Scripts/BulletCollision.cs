e4cfe1000c0d4c82aff15330a06ca777c22e6232
Mochammad Mustakim <mumustakim7@gmail.com>
2019-06-19 17:17:00 +0700

    Update mekanik tembak dan collision player

---
+++ Assets/Scripts/BulletCollision.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCollision : MonoBehaviour
+{
+    LifePlayerController lifePlayerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.name == "player2(Clone)")
+        {
+            lifePlayerController.lifePlayer[1]++;
+
+        } else if (col.gameObject.name == "player1(Clone)")
+        {
+            lifePlayerController.lifePlayer[0]++;
+        }
+    }
+}
---
+++ Assets/Scripts/LifePlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePlayerController : MonoBehaviour
+{
+    public int[] lifePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifePlayer[0] = 0;
+        lifePlayer[1] = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Player 1 : " + lifePlayer[0]);
+        Debug.Log("Player 2 : " + lifePlayer[1]);
+    }
+}
